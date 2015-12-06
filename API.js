2dc60bfb2f2d48cb16c6fe5b7d2de85ad47f4bf5
Yiğit YOL <ayigitd@gmail.com>
2015-12-06 03:03:21 +0000

    URI config

--- API.js
+++ API.js
@@ -1,23 +1,33 @@
 var express = require('express');
-//var db = require('./db.js');
+var mongoClient = require('mongodb').MongoClient;
 var app = express();
-//var connection = db();
+//REQs
 
+var URI = require('./URI.js')(); // CONNECTION STRING TO MONGODB
+//CONFIGs
+
+
+//APP START
 app.set('port', (process.env.PORT || 1071)); // port configuration
 
+//GETs START
 app.get("/",function(request, response){
-		response.send("Welcome");
-	
+	console.log("reach to / -GET");
+	response.send("Welcome");
 });
 
+//GETs END
 
+//POSTs START
 app.post("/register",function(request, response){
-	
+    console.log("reach to /register -POST");
 	
 });
+//POSTs END
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
+//APP END
 
