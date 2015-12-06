461efaeed51f4a71ae679922ea304b3b4951e02e
Yiğit YOL <ayigitd@gmail.com>
2015-12-06 03:28:40 +0000

    URI config test heroku

--- API.js
+++ API.js
@@ -28,6 +28,7 @@ app.post("/register",function(request, response){
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log(URI);
 });
 //APP END
 
