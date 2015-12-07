65b898a58220bb6343f5ef5cd62243554e6a1a44
Yiğit YOL <ayigitd@gmail.com>
2015-12-07 01:12:56 +0000

    Register Function added

--- API.js
+++ API.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var mongoClient = require('mongodb').MongoClient;
 var app = express();
+var assert = require("assert");
+var bodyParser = require("body-parser");
+var ObjectId = require("mongodb").ObjectId;
 //REQs
 
 var URI = require('./URI.js')(); // CONNECTION STRING TO MONGODB
+app.use(bodyParser.json()); // application/json REQUEST BODY PARSER
 //CONFIGs
 
 
@@ -22,7 +26,43 @@ app.get("/",function(request, response){
 //POSTs START
 app.post("/register",function(request, response){
     console.log("reach to /register -POST");
-	response.send(URI);
+    console.log("Params : " + JSON.stringify(request.body));
+    if(!request.is("json")){
+        response.status(400);
+    }
+    else{
+        try{
+            var username = request.body["USERNAME"];
+            var mail = request.body["MAIL"];
+            var password = request.body["PASSWORD"];
+           
+            var insertDocument = function(db, callback){
+               db.collection("USER").insertOne({
+                   "USERNAME" : username,
+                   "MAIL" : mail,
+                   "PASSWORD" : password,
+                   "DATE" : new Date(),
+                   "VALID" : false,
+                   "LAST_ACCESS" : null
+               }, function(err,result){
+                   assert.equal(err,null);
+                   console.log("User created. Detail : username " + 
+                   username + " mail " + mail );
+                   callback(result);
+               });
+            };
+        }
+        catch(e){
+            console.log("Internal server error -> Expection details :" + JSON.stringify(e));
+        }
+        mongoClient.connect(URI,function(err,db){
+            assert.equal(null, err);
+            insertDocument(db, function(){
+                response.send("Object created!");
+                db.close();
+            });
+        });
+    }
 });
 //POSTs END
 
--- testing/testing-c9.py
+++ testing/testing-c9.py
@@ -1,7 +1,12 @@
 import webbrowser,json,httplib
 connection = httplib.HTTPConnection('0.0.0.0',8080)
+headers = {'Content-Type': 'application/json'}
 connection.connect()
-connection.request("POST","/register")
+connection.request("POST","/register/",json.dumps({
+    "USERNAME": "TESTUSER",
+    "MAIL":"testuser@mail.com",
+    "PASSWORD":"123"})
+    ,headers)
 
 results = connection.getresponse().read()
 print results
